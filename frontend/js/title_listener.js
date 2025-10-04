5bda19b5c965e95fd2ad541791beea488c736aa6
Mathieu Poirier <mathieuworkemail@gmail.com>
2025-10-04 18:36:11 -0400

    Save point to  have frontend work with button

--- frontend/js/title_listener.js
+++ frontend/js/title_listener.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   const title = document.querySelector('.title-t');
   const sub = document.querySelector('.title-t2');
+  const title_btn = document.querySelector('.title-btn');
 
   title.addEventListener('animationend', () => {
-    sub.style.animation = 'fadeIn 2s ease-in-out forwards';
+    sub.style.animation = 'fadeIn 1.25s ease-in-out forwards';
+  });
+  sub.addEventListener('animationend', () => {
+    title_btn.style.animation = 'fadeIn 1.25s ease-in-out forwards';
   });
 });
