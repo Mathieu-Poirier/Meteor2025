f847a6e915654145059164f44d12494fe72cfa3f
Mathieu Poirier <mathieuworkemail@gmail.com>
2025-10-04 16:41:51 -0400

    Added Orbitron front and working title card animation

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,4 +1,10 @@
-import * as THREE from 'three';
+document.addEventListener('DOMContentLoaded', () => {
+  const title = document.querySelector('.title-t');
+  const sub = document.querySelector('.title-t2');
 
+  title.addEventListener('animationend', () => {
+    console.log('First animation done');
+    sub.style.animation = 'fadeIn 2s ease-in-out forwards';
+  });
+});
 
- 
